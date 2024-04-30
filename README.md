88ec8fd55f1af747acb872a9020f44af43453647
Sumeiya Abdi <sumeiya619@gmail.com>
2024-04-30 14:58:46 -0500

    looped through employee array and calculated bonus percentage

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 You are going to write a single JavaScript file to automatically calculate employee bonus for a company.
 
+
+H
 ## Objectives
 
 - Practice Read, Write, Speak, Teach skills
@@ -71,8 +73,7 @@ Write a declared function that takes in one **Employee** object (as an argument 
 - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
 - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
 - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
-- If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
-and should receive an additional 5%.
+- If their employee number is 4 digits long, this means they have been with the company for longer than 15 years, and should receive an additional 5%.
 - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
 - No bonus can be above 13% or below 0% total.
 
--- client.js
+++ client.js
@@ -34,6 +34,66 @@ const employees = [
 
 console.log('array of employee data: ',  employees );
 
+function calculateBonus(individual){
+let bonusPercentage = 0; 
+  if(individual.reviewRating === 3){
+    bonusPercentage = 0.04;
+} 
+  if (individual.reviewRating === 4){
+    bonusPercentage = 0.06;
+}
+  if (individual.reviewRating === 5){
+    bonusPercentage = 0.1;
+}
+
+let totalBonus = bonusPercentage *individual.annualSalary;
+let totalCompensation = 0;
+
+  let newObject ={
+    name: individual.name,
+    bonusPercntage: bonusPercentage,
+    totalBonus: totalBonus,
+    totalCompensation: totalCompensation
+
+  }
+return newObject;
+}
+
+
+// Loop over the employees array
+//at each object in the array, pass the object into a function
+//the function will return a new object
+
+
+
+// Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+//Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+//Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+
+// Those who have a rating of a 2 or below should not receive a bonus.
+
+
+//If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+//and should receive an additional 5%.
+//However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+//No bonus can be above 13% or below 0% total.
+
+let totalBonus = 0;
+if (individual.employeeNumber < )
+
+
+function listOfEmployees(){
+ for (let employee of employees){
+  console.log('New Object', calculateBonus(employee));
+  
+
+
+
+ }
+}
+
+listOfEmployees(employees);
+
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
